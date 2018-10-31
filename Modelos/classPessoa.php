f3721906b527e1dcec2c7219f6d2afbc32fc5635
Felipe S. Rodrigues <f.questao@gmail.com>
2018-10-30 22:23:53 -0300

    <@rfelipe>registro de novo usuario

--- Modelos/classPessoa.php
+++ Modelos/classPessoa.php
@@ -2,6 +2,7 @@
 class Pessoa {
     private static $codPessoa;
     private static $nomePessoa;
+    private static $senha;
    
    
     static function set_codPessoa($codPessoa) {
@@ -11,11 +12,18 @@ class Pessoa {
     static function set_nomePessoa($nomePessoa){
     	self::$nomePessoa = $nomePessoa;
     }
+    static function set_senha($senha){
+        self::$senha = $senha;
+    }
+
     function get_codPessoa(){
     	return self::$codPessoa;
     }
     function get_nomePessoa(){
     	return self::$nomePessoa;
+    }
+    function get_senha(){
+        return self::$senha;
     }
 }
 ?>
